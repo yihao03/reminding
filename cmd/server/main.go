7d580326d041bd59914c99b0beedf83cdf40e266
yihao <yihao039@gmail.com>
2025-12-04 16:27:46 +0800

    Update main

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,26 +1,37 @@
 package main
 
 import (
-	"log"
+	"log/slog"
 	"net/http"
 	"time"
 
+	"github.com/yihao03/reminding/internal/firebase"
 	"github.com/yihao03/reminding/internal/router"
 )
 
 const (
-	READ_HEADER_TIMEOUT_SEC = 5
+	READ_HEADER_TIMEOUT_SEC = 5 //nolint:gosec
 )
 
 func main() {
-	r := router.Setup()
+	slog.Info("Starting server...")
+
+	app, err := firebase.InitFirebase()
+	if err != nil {
+		slog.Error("Error initializing firebase", "error", err)
+	}
+
+	r := router.Setup(app)
+
 	server := &http.Server{
-		Addr:              "8080",
+		Addr:              ":8080",
 		Handler:           r,
 		ReadHeaderTimeout: READ_HEADER_TIMEOUT_SEC * time.Second,
 	}
-	err := server.ListenAndServe()
-	if err != nil {
-		log.Fatalln(err)
+
+	slog.Info("Listening on :8080")
+	if err := server.ListenAndServe(); err != nil {
+		slog.Error("Server failed to start: %v", "error", err)
+		panic(err)
 	}
 }
