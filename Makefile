15cca57d9d37199cfe73433aedf499500ce2b9d9
yihao <yihao039@gmail.com>
2025-12-04 11:08:55 +0800

    add linter config

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
 SERVER_PATH=./cmd/server/main.go
 
-run: 
-	air ${SERVER_PATH}
+run:
+	@air ${SERVER_PATH}
--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,4 +1,26 @@
-package server
+package main
+
+import (
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/yihao03/reminding/internal/router"
+)
+
+const (
+	READ_HEADER_TIMEOUT_SEC = 5
+)
 
 func main() {
+	r := router.Setup()
+	server := &http.Server{
+		Addr:              "8080",
+		Handler:           r,
+		ReadHeaderTimeout: READ_HEADER_TIMEOUT_SEC * time.Second,
+	}
+	err := server.ListenAndServe()
+	if err != nil {
+		log.Fatalln(err)
+	}
 }
