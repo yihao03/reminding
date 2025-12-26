066a679ed51797fe6b2c0b8ed2c1a43b80b034be
yihao <yihao039@gmail.com>
2025-12-27 00:19:49 +0800

    Add admin list event

---
+++ internal/handlers/admin/events/list.go
@@ -0,0 +1,28 @@
+package events
+
+import (
+	"net/http"
+
+	firebase "firebase.google.com/go/v4"
+	"github.com/yihao03/reminding/apperrors"
+	"github.com/yihao03/reminding/internal/api"
+	"github.com/yihao03/reminding/internal/database/sqlc"
+	"github.com/yihao03/reminding/internal/views/eventview"
+)
+
+const (
+	ErrGetEvents     = "Error getting events"
+	SuccessGetEvents = "Events retrieved successfully"
+)
+
+func HandleGetEvents(w http.ResponseWriter, r *http.Request, queries *sqlc.Queries, app *firebase.App) error {
+	events, err := queries.ListEvents(r.Context())
+	if err != nil {
+		return apperrors.Wrap(err, ErrGetEvents)
+	}
+
+	view := eventview.ToEventListView(&events)
+
+	api.WriteResponse(view, w, SuccessGetEvents)
+	return nil
+}
