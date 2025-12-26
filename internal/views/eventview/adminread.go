93df0061da9c5aab367e6542cdb0413237845408
yihao <yihao039@gmail.com>
2025-12-26 21:59:05 +0800

    add admin views

---
+++ internal/views/eventview/adminread.go
@@ -0,0 +1,35 @@
+package eventview
+
+import (
+	"time"
+
+	"github.com/yihao03/reminding/internal/database/sqlc"
+	"github.com/yihao03/reminding/internal/views/userview"
+)
+
+type EventAdminView struct {
+	ID           int32               `json:"id"`
+	Organiser    string              `json:"organiser"`
+	IsOnline     bool                `json:"isOnline"`
+	LocationName string              `json:"locationName"`
+	StartTime    time.Time           `json:"startTime"`
+	EndTime      time.Time           `json:"endTime"`
+	EventName    string              `json:"eventName"`
+	CreatedAt    time.Time           `json:"createdAt"`
+	Details      string              `json:"details"`
+	Users        []userview.UserView `json:"users"`
+}
+
+func ToAdminEventView(event *sqlc.Event, users *[]sqlc.User) *EventAdminView {
+	return &EventAdminView{
+		ID:           event.ID,
+		Organiser:    event.Organiser.String,
+		IsOnline:     event.IsOnline,
+		LocationName: event.LocationName.String,
+		StartTime:    event.StartTime.Time,
+		EndTime:      event.EndTime.Time,
+		EventName:    event.EventName,
+		CreatedAt:    event.CreatedAt.Time,
+		Details:      event.Details.String,
+	}
+}
