bb4fa186365f5f97c5b591e470b132d0bdfdc189
yihao <yihao039@gmail.com>
2025-12-26 21:59:26 +0800

    Add admin read events

---
+++ internal/handlers/admin/events/read.go
@@ -0,0 +1,43 @@
+package events
+
+import (
+	"net/http"
+	"strconv"
+
+	firebase "firebase.google.com/go/v4"
+	"github.com/go-chi/chi/v5"
+	"github.com/yihao03/reminding/apperrors"
+	"github.com/yihao03/reminding/internal/api"
+	"github.com/yihao03/reminding/internal/database/sqlc"
+	"github.com/yihao03/reminding/internal/views/eventview"
+)
+
+var ErrMissingIDParam = "missing id parameter"
+
+func HandleReadEvents(w http.ResponseWriter, r *http.Request, queries *sqlc.Queries, app *firebase.App) error {
+	id := chi.URLParam(r, "id")
+	if id == "" {
+		api.WriteError(http.StatusBadRequest, apperrors.Wrap(nil, ErrMissingIDParam), w, r.Context())
+		return nil
+	}
+
+	intID, err := strconv.Atoi(id)
+	if err != nil {
+		api.WriteError(http.StatusBadRequest, apperrors.Wrap(err, ErrMissingIDParam), w, r.Context())
+		return nil
+	}
+
+	event, err := queries.GetEventById(r.Context(), int32(intID))
+	if err != nil {
+		return apperrors.NewInternalError(err, "failed to get event by id")
+	}
+
+	users, err := queries.GetEventRegisteredUsers(r.Context(), int32(intID))
+	if err != nil {
+		return apperrors.NewInternalError(err, "failed to get event registered users")
+	}
+
+	eventView := eventview.ToAdminEventView(&event, &users)
+	api.WriteResponse(eventView, w)
+	return nil
+}
