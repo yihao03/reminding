d027b47af0463881dd9d014c6661db1fb2602a5d
yihao <yihao039@gmail.com>
2025-12-30 21:46:46 +0800

    Update view to include new fields

--- internal/views/userview/create.go
+++ internal/views/userview/create.go
@@ -1,11 +1,16 @@
 package userview
 
-import "github.com/yihao03/reminding/internal/database/sqlc"
+import (
+	"github.com/yihao03/reminding/internal/database"
+	"github.com/yihao03/reminding/internal/database/sqlc"
+)
 
 type CreateUserView struct {
 	FirebaseUID string `json:"uid" validate:"required"`
 	Email       string `json:"email" validate:"required,email"`
 	DisplayName string `json:"displayName" validate:"required,max=100"`
+	State       string `json:"state" validate:"oneof='Johor' 'Kedah' 'Kelantan' 'Melaka' 'Negeri Sembilan' 'Pahang' 'Perak' 'Perlis' 'Penang' 'Sabah' 'Sarawak' 'Selangor' 'Terengganu'"`
+	Age         int32  `json:"age" validate:"min=0,max=120"`
 }
 
 func (v *CreateUserView) ToCreateUserParams() *sqlc.CreateUserParams {
@@ -13,5 +18,6 @@ func (v *CreateUserView) ToCreateUserParams() *sqlc.CreateUserParams {
 		FirebaseUid: v.FirebaseUID,
 		Email:       v.Email,
 		DisplayName: v.DisplayName,
+		Age:         database.ToPGInt4(&v.Age),
 	}
 }
